2006877207f4b14a4742cc4d2a4914441ed9fe6b
atcl <atcl@36662c96-6cfc-4e72-a30f-691a4a54763d>
2009-04-23 23:26:10 +0000

    git-svn-id: svn://graasmilk.net/atcl/xizero@9 36662c96-6cfc-4e72-a30f-691a4a54763d

--- src/inc/CLmisc.hh
+++
@@ -1,9 +0,0 @@
-//atCROSSLEVEL studios 2009
-//licensed under zlib/libpng license
-#ifndef HH_CLMISC
-#define HH_CLMISC
-#pragma message "Compiling " __FILE__ " ! TODO: all"
-
-
-
-#endif
--- src/xmp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-g++ -I/usr/include/freetype2 -o xizero xizero.cc -lfltk -Wno-multichar -Wno-write-strings -O3
