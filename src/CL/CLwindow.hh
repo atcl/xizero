f93ec35a9d743b6ca119b613d6f238d730e4a17c
atcl <atcl@TURBINE.(none)>
2009-07-16 23:22:10 +0200

    new CLwindow for testing fastgl

--- src/CL/CLwindow.hh
+++ src/CL/CLwindow.hh
@@ -1,161 +1,85 @@
-//atCROSSLEVEL studios 2009
-//licensed under zlib/libpng license
-#ifndef HH_CLWINDOW
-#define HH_CLWINDOW
-#pragma message: "Compiling " __FILE__ " ! TODO: "
-
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
+#include <fastgl/fastgl.h>
 
 #include "CLtypes.hh"
 
-
 namespace CLwindow
 {
-	//
-	Display* d;
-	Window   w;
-	Atom     a;
-	XEvent   e;
-	Screen*  s;
-	Visual*  v;
-	XImage*  i;
-	GC       g;
-	int      n;
+	fgl::FGApp* 		app;
+	fgl::FGWindow* 		win;
+	fgl::FGDrawBuffer* 	vram;
 	
-	//
-	xchar  kp;
-	xchar  kt;
-	xlong  mx;
-	xlong  my;
-	xlong  mb;
-	xlong  mt;
-	xchar* vm;
-	xlong* vb;
+	xlong key;
+	xlong tkey;
+	xlong mousex;
+	xlong mousey;
+	xlong mouseb;
 	
-	//
-	void   init();
-	void   flush();
-	void   close();
-	void   getinput();
-	xchar  getkey();
-	xchar  getturbokey();
-	xlong  getmousex();
-	xlong  getmousey();
-	xlong  getmouseb();
-	xlong* getvideobuffer();
-	void   settitle(const xchar* c);
-	void   drawtext(xlong x,xlong y,const xchar* s,xlong c);
-}
-
-void CLwindow::init()
-{
-	d = XOpenDisplay(NULL);
-
-	s = DefaultScreenOfDisplay(d);
-	n = DefaultScreen(d);
-	v = DefaultVisualOfScreen(s);
-
-	w = XCreateWindow(d,DefaultRootWindow(d),0,0,xres,yres,0,24,InputOutput,v,0,NULL);
-
-	a = XInternAtom(d,"WM_DELETE_WINDOW",true);
-	if(a) XSetWMProtocols(d,w,&a,1);
-
-	XMapWindow(d,w);
-
-	g = XCreateGC(d,w, 0, NULL);
-
-	vm = new xchar[xres*yres*4];
-	vb = reinterpret_cast<xlong*>(&vm[0]);
-
-	i = XCreateImage(d,v,24,ZPixmap,0,vm,xres,yres,32,xres*4);
+	void init(xlong w,xlong h,const xchar* t,int argc, char *argv[]);
+	void run();
+	void flush(xlong* db);
+	void exit();
+	void update(fgl::FGEvent *e);
 	
-	XSelectInput(d,w, ExposureMask | KeyPressMask | KeyReleaseMask | ButtonPressMask | ButtonReleaseMask | PointerMotionMask);
+	xlong getkey();
+	xlong turbokey();
+	xlong getmousex();
+	xlong getmousey();
+	xlong getmouseb();
+	
+	
+	
+};
+
+
+void CLwindow::init(xlong w,xlong h,const xchar* t,int argc, char *argv[])
+{
+	app = new fgl::FGApp(fgl::G800x600,argc,argv,fgl::CBLACK,fgl::APP_ENABLEALTX);
+	win = new fgl::FGWindow(0, 0,0,w,h,t,CLwindow::update);
+	vram = new fgl::FGDrawBuffer(w,h);	
 }
 
-void CLwindow::flush()
+void CLwindow::run()
 {
-	XPutImage(d,w,g,i,0,0,0,0,xres,yres);
-	XFlush(d);
+	app->Run();
 }
 
-void CLwindow::close()
+void CLwindow::flush(xlong* db)
 {
-	XDestroyImage(i);
-	XFreeGC(d,g);
-	XDestroyWindow(d,w);
-	XCloseDisplay(d);
+	win->WindowPutBitmap(0,0,0,0,800,600,vram);
 }
 
-void CLwindow::getinput()
+void CLwindow::exit()
 {
-	XNextEvent(d,&e);
-	switch(e.type)
+	fgl::FGApp::AppDone();
+}
+
+void CLwindow::update(fgl::FGEvent *e)
+{
+	switch(e->GetType())
 	{
-		case KeyPress:		kp = XKeycodeToKeysym(d,e.xkey.keycode,0);
-							break;
+		case fgl::INITEVENT:
+			//on start
+			break;
+			
+		case fgl::KEYEVENT:
 
-		case KeyRelease:	kt = XKeycodeToKeysym(d,e.xkey.keycode,0);
-							break;
+			break;
+			
+		case fgl::MOVEEVENT:
 
-		case ButtonPress:	mb = e.xbutton.button;
-							mx = e.xbutton.x;
-							my = e.xbutton.y;
-							break;
+			break;
+			
+		case fgl::CLICKLEFTEVENT:
 
-		case ButtonRelease:	mt = e.xbutton.button;
-							mx = e.xbutton.x;
-							my = e.xbutton.y;
-							break;
-
-		case MotionNotify:	mx = e.xbutton.x;
-							my = e.xbutton.y;
-							break;
-
-//		case ClientMessage:	if(e.xclient.data.l[0] == a) close();
-//							break;	
+			break;
+			
+		case fgl::CLICKRIGHTEVENT:
+			
+			break;
+			
+		case fgl::TERMINATEEVENT:
+			//on exit
+			break;
 	}
 }
-
-xchar CLwindow::getkey()
-{
-	return kp;
-}
-
-xchar CLwindow::getturbokey()
-{
-	return kt;
-}
-
-xlong CLwindow::getmousex()
-{
-	return mx;
-}
-
-xlong CLwindow::getmousey()
-{
-	return my;
-}
-
-xlong CLwindow::getmouseb()
-{
-	return mb;
-}
-
-xlong* CLwindow::getvideobuffer()
-{
-	return vb;
-}
-
-void CLwindow::settitle(const xchar* c)
-{
-	XStoreName(d,w,c);
-}
-
-void CLwindow::drawtext(xlong x,xlong y,const xchar* s,xlong c)
-{
-	//XDrawString(d,d,g,x,y,c,length)
-}
-
-#endif
 
