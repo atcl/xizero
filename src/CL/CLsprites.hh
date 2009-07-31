12ead9da54209b42d8cf9c8abd9574b0d21d2a10
atcl <atcl@TURBINE.(none)>
2009-07-31 19:39:36 +0200

    antimatter compiled sprite

--- src/CL/CLsprites.hh
+++ src/CL/CLsprites.hh
@@ -17,7 +17,52 @@ namespace CLsprites
 
 void CLsprites::drawantimatter(xlong x,xlong y)
 {
-
+if( (x-2)<0 || (x+3)>=xres || (y-2)<0 || (y+3)>=yres) return;
+	
+	xlong l1 = ((y-2)*xres)+(x-1);
+	xlong l2 = l1 + xres - 1;
+	xlong l3 = l2 + xres;
+	xlong l4 = l3 + xres;
+	xlong l5 = l4 + xres;
+	xlong l6 = l5 + xres + 1; 
+	
+	(*CLdoublebuffer)[l1] = 0x00800080; l1++;
+	(*CLdoublebuffer)[l1] = 0x00800080; l1++;
+	(*CLdoublebuffer)[l1] = 0x00800080; l1++;
+	(*CLdoublebuffer)[l1] = 0x00800080;
+	
+	(*CLdoublebuffer)[l2] = 0x00800080; l2++;
+	(*CLdoublebuffer)[l2] = 0x00AD00AD; l2++;
+	(*CLdoublebuffer)[l2] = 0x00AD00AD; l2++;
+	(*CLdoublebuffer)[l2] = 0x00AD00AD; l2++;
+	(*CLdoublebuffer)[l2] = 0x00AD00AD; l2++;
+	(*CLdoublebuffer)[l2] = 0x00800080;
+	
+	(*CLdoublebuffer)[l3] = 0x00800080; l3++;
+	(*CLdoublebuffer)[l3] = 0x00AD00AD; l3++;
+	(*CLdoublebuffer)[l3] = 0x00FF00FF; l3++;
+	(*CLdoublebuffer)[l3] = 0x00FF00FF; l3++;
+	(*CLdoublebuffer)[l3] = 0x00AD00AD; l3++;
+	(*CLdoublebuffer)[l3] = 0x00800080;
+	
+	(*CLdoublebuffer)[l4] = 0x00800080; l4++;
+	(*CLdoublebuffer)[l4] = 0x00AD00AD; l4++;
+	(*CLdoublebuffer)[l4] = 0x00FF00FF; l4++;
+	(*CLdoublebuffer)[l4] = 0x00FF00FF; l4++;
+	(*CLdoublebuffer)[l4] = 0x00AD00AD; l4++;
+	(*CLdoublebuffer)[l4] = 0x00800080;
+	
+	(*CLdoublebuffer)[l5] = 0x00800080; l5++;
+	(*CLdoublebuffer)[l5] = 0x00AD00AD; l5++;
+	(*CLdoublebuffer)[l5] = 0x00AD00AD; l5++;
+	(*CLdoublebuffer)[l5] = 0x00AD00AD; l5++;
+	(*CLdoublebuffer)[l5] = 0x00AD00AD; l5++;
+	(*CLdoublebuffer)[l5] = 0x00800080;
+	
+	(*CLdoublebuffer)[l6] = 0x00800080; l6++;
+	(*CLdoublebuffer)[l6] = 0x00800080; l6++;
+	(*CLdoublebuffer)[l6] = 0x00800080; l6++;
+	(*CLdoublebuffer)[l6] = 0x00800080;
 }
 
 void CLsprites::drawplasma(xlong x,xlong y)
