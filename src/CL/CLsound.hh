b52edb12cb264178a833eab3f38f6a8eddc91502
atcl <atcl@TURBINE.(none)>
2009-08-05 12:42:05 +0200

    some sound preparations

--- src/CL/CLsound.hh
+++ src/CL/CLsound.hh
@@ -75,5 +75,80 @@ float CLsound::getvolume()
 	return volume;
 }
 
+//~ //basic wav player
+//~ namespace CLxsound
+//~ {
+	//~ xlong device;
+	//~ 
+	//~ bool init();
+	//~ bool play();
+	//~ void stop();
+	//~ void exit();
+//~ };
+//~ 
+//~ 
+//~ #ifdef WIN32
+//~ #include <mmsystem.hh>
+//~ 
+//~ bool init()
+//~ {
+	//~ device=0;
+	//~ return 0;
+//~ }
+//~ 
+//~ bool play(const xchar* f,bool l)
+//~ {
+	//~ switch(l)
+	//~ {
+		//~ case false:
+			//~ return PlaySound(TEXT(f), NULL, SND_FILENAME | SND_ASYNC);
+		//~ break;
+		//~ 
+		//~ case true:
+			//~ return PlaySound(TEXT(f), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+		//~ break;
+	//~ }
+//~ }
+//~ 
+//~ void stop()
+//~ {
+	//~ PlaySound(NULL, 0, 0);
+//~ }
+//~ 
+//~ void exit()
+//~ {
+	//~ 
+//~ }
+//~ #else //ifdef LINUX
+//~ #include <linux/soundcard.h>
+//~ #include <sys/ioctl.h>
+//~ 
+//~ bool init()
+//~ {
+	//~ device = open("/dev/dsp", O_WRONLY);
+//~ }
+//~ 
+//~ bool play(const xchar f,bool l)
+//~ {
+	//~ //load wav (header + data)
+	//~ //set bits per sample
+	//~ //set number of channels
+	//~ //set sample rate
+	//~ //start async playing (how?, maybe fork!)
+	//~ 
+//~ }
+//~ 
+//~ void stop()
+//~ {
+	//~ //stop async playing
+//~ }
+//~ 
+//~ void exit()
+//~ {
+	//~ close(device);
+//~ }
+//~ #endif
+//~ //*
+
 #endif
 
