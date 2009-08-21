ddd47bbec9998f7dccebdf13c883201b05e96a8a
atcl <atcl@TURBINE.(none)>
2009-08-21 20:04:00 +0200

    level-build-script fix

--- dat/level/arlevel.sh
+++ dat/level/arlevel.sh
@@ -1,9 +1,11 @@
 #!/bin/sh 
 
 if [ "$1" != "" ]; then
+	rm -f $1'.a'
 	ar -q $1'.a' $1'.mapt' $1'.maph' $1'.mape'
 else
 	echo -n "Enter level name: "
 	read level
+	rm -f $level'.a'
 	ar -q $level'.a' $level'.mapt' $level'.maph' $level'.mape'
 fi
