637f80949301d9cb82308c1d6025b1c7ececa963
atcl <atcl@TURBINE.(none)>
2009-08-27 20:59:07 +0200

    CLboool commented

--- src/CL/CLboool.hh
+++ src/CL/CLboool.hh
@@ -24,7 +24,7 @@ class boool
 		boool(xlong a);
 		~boool();
 	
-		inline boool& operator=(boool a);
+		inline boool& operator=(boool& a);
 		inline boool& operator=(bool a);
 		inline boool& operator=(xlong a);
 		
@@ -34,9 +34,9 @@ class boool
 		inline boool& operator++();
 		inline boool& operator--();
 		
-		inline boool& operator+=(boool a);
-		inline boool& operator-=(boool a);
-		inline boool& operator*=(boool a);
+		inline boool& operator+=(boool& a);
+		inline boool& operator-=(boool& a);
+		inline boool& operator*=(boool& a);
 		inline boool& operator+=(bool a);
 		inline boool& operator-=(bool a);
 		inline boool& operator*=(bool a);
@@ -44,9 +44,9 @@ class boool
 		inline boool& operator-=(xlong a);
 		inline boool& operator*=(xlong a);
 		
-		inline boool operator+(boool a) const;
-		inline boool operator-(boool a) const;
-		inline boool operator*(boool a) const;
+		inline boool operator+(boool& a) const;
+		inline boool operator-(boool& a) const;
+		inline boool operator*(boool& a) const;
 		inline boool operator+(bool a)  const;
 		inline boool operator-(bool a)  const;
 		inline boool operator*(bool a)  const;
@@ -54,20 +54,20 @@ class boool
 		inline boool operator-(xlong a) const;
 		inline boool operator*(xlong a) const;
 		
-		friend boool operator+(xlong a,boool b);
-		friend boool operator-(xlong a,boool b);
-		friend boool operator*(xlong a,boool b);
+		friend boool operator+(xlong a,boool& b);
+		friend boool operator-(xlong a,boool& b);
+		friend boool operator*(xlong a,boool& b);
 		
-		inline bool operator&&(boool a) const;
-		inline bool operator||(boool a) const;
-		inline bool operator^(boool a)  const;
+		inline bool operator&&(boool& a) const;
+		inline bool operator||(boool& a) const;
+		inline bool operator^(boool& a)  const;
 		
-		inline bool operator==(boool a) const;
-		inline bool operator!=(boool a) const;
-		inline bool operator<=(boool a) const;
-		inline bool operator>=(boool a) const;
-		inline bool operator<(boool a)  const;
-		inline bool operator>(boool a)  const;
+		inline bool operator==(boool& a) const;
+		inline bool operator!=(boool& a) const;
+		inline bool operator<=(boool& a) const;
+		inline bool operator>=(boool& a) const;
+		inline bool operator<(boool& a)  const;
+		inline bool operator>(boool& a)  const;
 		inline bool operator==(bool a)  const;
 		inline bool operator!=(bool a)  const;
 		inline bool operator<=(bool a)  const;
@@ -75,12 +75,12 @@ class boool
 		inline bool operator<(bool a)   const;
 		inline bool operator>(bool a)   const;
 		
-		friend bool operator==(bool a,boool b);
-		friend bool operator!=(bool a,boool b);
-		friend bool operator<=(bool a,boool b);
-		friend bool operator>=(bool a,boool b);
-		friend bool operator<(bool a,boool b);
-		friend bool operator>(bool a,boool b);
+		friend bool operator==(bool a,boool& b);
+		friend bool operator!=(bool a,boool& b);
+		friend bool operator<=(bool a,boool& b);
+		friend bool operator>=(bool a,boool& b);
+		friend bool operator<(bool a,boool& b);
+		friend bool operator>(bool a,boool& b);
 
 		inline operator bool()  const;		
 		inline operator xlong() const;
@@ -108,276 +108,373 @@ boool::boool(xlong a)
 
 boool::~boool() { }
 
-boool& boool::operator=(boool a)
+boool& boool::operator=(boool& a)
 {
 	this->b = a.b;
 }
 
+//assign bool
 boool& boool::operator=(bool a)
 {
 	this->b = a;
 	return *this;
 }
+//*
 
+//assign xlong
 boool& boool::operator=(xlong a)
 {
 	this->b = CLmath::sign(a);
 	return *this;
 }
+//*
 
+//negate
 boool boool::operator-() const
 {
 	boool temp(-(this->b));
 	return temp;
 }
+//*
 
+//invert
 boool boool::operator!() const
 {
 	boool temp(-(this->b));
 	return temp;
 }
+//*
 	
+//increment
 boool& boool::operator++()
 {
 	if(this->b<1) this->b++;
 	return *this;
 }
+//*
 
+//decrement
 boool& boool::operator--()
 {
 	if(this->b>-1) this->b--;
 	return *this;
 }
+//*
 
-boool& boool::operator+=(boool a)
+//assignment addition with boool
+boool& boool::operator+=(boool& a)
+{
+	
+}
+//*
+
+//assignment subtraction with boool
+boool& boool::operator-=(boool& a)
+{
+	
+}
+//*
+
+//assignment multiplication with boool
+boool& boool::operator*=(boool& a)
 {
 	
 }
 
-boool& boool::operator-=(boool a)
-{
-	
-}
-
-boool& boool::operator*=(boool a)
-{
-	
-}
-
+//assignment addition with bool
 boool& boool::operator+=(bool a)
 {
 	
 }
+//*
 
+//assignment subtraction with bool
 boool& boool::operator-=(bool a)
 {
 	
 }
+//*
 
+//assignment multiplication with bool
 boool& boool::operator*=(bool a)
 {
 	
 }
+//*
 
+//assignment addition with xlong
 boool& boool::operator+=(xlong a)
 {
 	
 }
+//*
 
+//assignment subtraction with xlong
 boool& boool::operator-=(xlong a)
 {
 	
 }
+//*
 
+//assignment multiplication with xlong
 boool& boool::operator*=(xlong a)
 {
 	
 }
+//*
 
-boool boool::operator+(boool a) const
+//add with boool
+boool boool::operator+(boool& a) const
 {
 	boool temp(CLmath::sign(this->b + a.b));
 	return temp;
 }
+//*
 
-boool boool::operator-(boool a) const
+//subtract with boool
+boool boool::operator-(boool& a) const
 {
 	boool temp(CLmath::sign(this->b - a.b));
 	return temp;
 }
 
-boool boool::operator*(boool a) const
+//multiplicate with bool
+boool boool::operator*(boool& a) const
 {
 	boool temp(CLmath::sign(this->b * a.b));
 	return temp;
 }
+//*
 
+//add with bool
 boool boool::operator+(bool a)  const
 {
 	boool temp(CLmath::sign(this->b + xlong(a)));
 	return temp;
 }
+//*
 
+//subtract with bool
 boool boool::operator-(bool a)  const
 {
 	boool temp(CLmath::sign(this->b - xlong(a)));
 	return temp;	
 }
+//*
 
+//multiplicate with bool
 boool boool::operator*(bool a)  const
 {
 	boool temp(CLmath::sign(this->b * xlong(a)));
 	return temp;	
 }
+//*
 
+//add with xlong
 boool boool::operator+(xlong a) const
 {
 	boool temp(CLmath::sign(this->b + a));
 	return temp;	
 }
+//*
 
+//subtract with xlong
 boool boool::operator-(xlong a) const
 {
 	boool temp(CLmath::sign(this->b - a));
 	return temp;	
 }
+//*
 
+//multiplicate with xlong
 boool boool::operator*(xlong a) const
 {
 	boool temp(CLmath::sign(this->b + a));
 	return temp;
 }
-		
-bool boool::operator&&(boool a) const
+//*
+	
+//logical and with boool	
+bool boool::operator&&(boool& a) const
 {
 	
+}
+//*
+
+//logical or vboool
+bool boool::operator||(boool& a) const
+{
+	
+}
+//*
+
+//logical xor with boool
+bool boool::operator^(boool& a)  const
+{
+	
+}
+//*
+
+//equality with boool		
+bool boool::operator==(boool& a) const
+{
+	return (this->b == a.b);
+}
+//*
+
+//inequality with boool
+bool boool::operator!=(boool& a) const
+{
+	return (this->b != a.b);
+}
+//*
+
+//less or equal with boool
+bool boool::operator<=(boool& a) const
+{
+	return (this->b <= a.b);
+}
+//*
+
+//greater or equal with boool
+bool boool::operator>=(boool& a) const
+{
+	return (this->b >= a.b);
+}
+//*
+
+//less with boool
+bool boool::operator<(boool& a)  const
+{
+	return (this->b < a.b);
+}
+//*
+
+//greater with boool
+bool boool::operator>(boool& a)  const
+{
+	return (this->b > a.b);
 }
 
-bool boool::operator||(boool a) const
-{
-	
-}
-
-bool boool::operator^(boool a)  const
-{
-	
-}
-		
-bool boool::operator==(boool a) const
-{
-	
-}
-
-bool boool::operator!=(boool a) const
-{
-	
-}
-
-bool boool::operator<=(boool a) const
-{
-	
-}
-
-bool boool::operator>=(boool a) const
-{
-	
-}
-
-bool boool::operator<(boool a)  const
-{
-	
-}
-
-bool boool::operator>(boool a)  const
-{
-	
-}
-
+//equality with bool
 bool boool::operator==(bool a)  const
 {
-	
+	return (this->b == a);
 }
+//*
 
+//inequality with bool
 bool boool::operator!=(bool a)  const
 {
-	
+	return (this->b == a);
 }
+//*
 
+//less or equal with bool
 bool boool::operator<=(bool a)  const
 {
-	
+	return (this->b <= a);
 }
+//*
 
+//greater or equal with bool
 bool boool::operator>=(bool a)  const
 {
-	
+	return (this->b >= a);
 }
+//*
 
+//less with bool
 bool boool::operator<(bool a)   const
 {
-	
+	return (this->b < a);
 }
+//*
 
+//greater with bool
 bool boool::operator>(bool a)   const
 {
-	
+	return (this->b > a);
 }
+//*
 
+//cast to bool
 boool::operator bool()  const
 {
 	return bool(this->b);
 }
-	
+//*
+
+//cast to xlong
 boool::operator xlong() const
 {
 	return b;
 }
+//*
 
 //friends:
 
-inline boool operator+(xlong a,boool b)
+//add with xlong from left
+inline boool operator+(xlong a,boool& b)
 {
 	
 }
+//*
 
-inline boool operator-(xlong a,boool b)
+//subtract with xlong from left
+inline boool operator-(xlong a,boool& b)
 {
 	
 }
+//*
 
-inline boool operator*(xlong a,boool b)
+//multiplicate with xlong from left
+inline boool operator*(xlong a,boool& b)
 {
 	
 }
+//*
 
-inline bool operator==(bool a,boool b)
+//equality with bool from left
+inline bool operator==(bool a,boool& b)
 {
 	
 }
+//*
 
-inline bool operator!=(bool a,boool b)
+//inequality with bool from left
+inline bool operator!=(bool a,boool& b)
 {
 	
 }
+//*
 
-inline bool operator<=(bool a,boool b)
+//less or equal with bool from left
+inline bool operator<=(bool a,boool& b)
 {
 	
 }
+//*
 
-inline bool operator>=(bool a,boool b)
+//greater or equal with bool from left
+inline bool operator>=(bool a,boool& b)
 {
 	
 }
+//*
 
-inline bool operator<(bool a,boool b)
+//less with bool from left
+inline bool operator<(bool a,boool& b)
 {
 	
 }
+//*
 
-inline bool operator>(bool a,boool b)
+//greater with bool from left
+inline bool operator>(bool a,boool& b)
 {
 	
 }
+//*
 
 #endif
--- src/CL/CLdetect.hh
+++ src/CL/CLdetect.hh
@@ -6,26 +6,97 @@
 
 #include "CLtypes.hh"
 
+//cpucaps constants
+#define FPU   0b00000000000000000000000000000001
+#define MMX   0b00000000000000000000000000000010
+#define SSE   0b00000000000000000000000000000100
+#define SSE2  0b00000000000000000000000000000000
+#define SSE3  0b00000000000000000000000000000000
+#define SSE41 0b00000000000000000000000000000000
+#define SSE42 0b00000000000000000000000000000000
+#define SSE5a 0b00000000000000000000000000000000
+
+//*
 
 namespace CLdetect
 {
-	void detectCPU(bool& mmx,bool& sse);
-	void detectram();
-	void detectgpu();
-	void detectvram();
-	void detectsb();
-	void detectps();
+	xlong cpu();
+	xlong ram();
+	xchar* gpu();
+	xlong vram();
+	xchar* sb();
+	xchar ps();
 };
 
-void detectCPU(bool& mmx,bool& sse)
+xlong CLdetect::cpu()
 {
 	xlong dd;
 	
 	__asm__ __volatile__ ("cpuid":"=d"(dd):"a"(1));
 	
-	if(dd && MMXFLAG) mmx=1;
-	if(dd && SSEFLAG) sse=1;
+	xlong r = 0;
+	
+	if(dd && MMXFLAG) r += MMX;
+	if(dd && SSEFLAG) r += SSE;
+	
+	return r;
 }
+
+#ifdef WIN32
+
+xlong CLdetect::ram()
+{
+	
+}
+
+xchar* CLdetect::gpu()
+{
+	
+}
+
+xlong CLdetect::vram()
+{
+	
+}
+
+xchar* CLdetect::sb()
+{
+	
+}
+
+xlong CLdetect::ps()
+{
+	
+}
+
+#else //ifdef LINUX
+
+xlong CLdetect::ram()
+{
+	
+}
+
+xchar* CLdetect::gpu()
+{
+	
+}
+
+xlong CLdetect::vram()
+{
+	
+}
+
+xchar* CLdetect::sb()
+{
+	
+}
+
+xlong CLdetect::ps()
+{
+	
+}
+
+#endif 
 
 
 #endif
