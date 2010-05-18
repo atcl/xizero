975976999ea4e0739d160343f45e8976f9977ba9
atcl <atcl@atcrosslevel.de>
2010-05-18 22:27:59 +0200

    CLvoxels.hh

---
+++ src/CL/CLvoxels.hh
@@ -0,0 +1,59 @@
+///license
+//atCROSSLEVEL studios 2009,2010
+//licensed under zlib/libpng license
+///*
+
+///guard
+#ifndef HH_CLVOXEL
+#define HH_CLVOXEL
+///*
+
+///includes
+#include "CLtypes.hh"
+#include "CLbase.hh"
+#include "CLvector.hh"
+///*
+
+///header
+/* class name:	CLvoxel
+ * 
+ * description:	
+ * 
+ * author:		atcl
+ * 
+ * notes:		...
+ * 
+ * version: 	0.2
+ */
+///*
+
+///definitions
+struct CLvoxel
+{
+	CLfvector position;
+	CLfvector normal;
+	uxlong    color;
+};
+
+class CLvoxels : public CLbase<CLid,0>
+{
+	friend class CLbase<CLvoxels,0>;
+	
+	private:
+		
+	protected:
+		CLvoxel** voxel;
+	public:
+		CLvoxel() { };
+		~CLvoxel() { };
+
+};
+///*
+
+///implementation
+
+
+
+///*
+
+#endif
